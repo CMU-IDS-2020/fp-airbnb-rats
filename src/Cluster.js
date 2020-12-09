cf90d7922869fc73f82a1b4a087f1abb035faf82
shravb <70634053+shravb@users.noreply.github.com>
2020-12-09 13:54:26 +0530

    clustering integration

--- src/Cluster.js
+++ src/Cluster.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Box } from "jsxstyle";
+import $ from 'jquery';
+
+var option_global = 'type'
+var flaskAppURL = 'http://ec2-54-198-69-44.compute-1.amazonaws.com/'
+var yourGroups = { 0: [0, 1, 2, 3, 4, 5], 1: [6, 7, 8, 9] };
 
 class Cluster extends Component {
   constructor(props) {
@@ -14,28 +19,266 @@ class Cluster extends Component {
   }
 
   sendToApp() {
-    const yourGroups = { 0: [0, 1, 2, 3, 4, 5], 1: [6, 7, 8, 9] };
+    console.log("inside sendToApp");
+    console.log(yourGroups);
     this.props.changeDataGroups(yourGroups);
   }
 
-  sendToBackend() {
-    alert("The state contains " + this.state.value);
+  async getData(elmnt, params) {
+    var url = ""
+    var K = 5
+
+    if (elmnt.id == "km") {
+      K= params[0];
+      var pca = params[1];
+      var variance = params[2];
+      var perplexity = params[3];
+      var params_url = "kmeans?K="+K+"&pca="+pca+"&variance="+variance+"&perplexity="+perplexity;
+      var url = flaskAppURL + params_url;
+    }
+
+    else if (elmnt.id == "hc") {
+      K = params[0];
+      var linkage = params[1];
+      var params_url = "hierarchical?num="+K+"&linkage="+linkage;
+      var url = flaskAppURL + params_url
+    }
+
+    else {
+      console.log("hello");
+      var maxmin = params[0];
+      var params_url = "none?type="+maxmin;
+      var url = flaskAppURL + params_url;
+    }
+    console.log(url);
+
+    var x = 0;
+    var result = await $.ajax({
+         url: url,
+         type: 'GET',
+         success: function (data) {
+             var response = JSON.parse(data);
+             yourGroups = response;
+             console.log("inside success function")
+             console.log(yourGroups);
+             x = 1;
+         },
+         error: function (error) {
+             console.log(error);
+             x = 1;
+         }
+    });
+  }
+
+  async sendToBackend() {
+    var params = [];
+    if (option_global == 'km') {
+      console.log("kmeans submitted");
+      var k = $('input[name="k"]').val();
+      var dr = $('input[name="dr"]:checked').val();
+      var variance = $('input[name="variance"]').val();
+      var perplexity = $('input[name="perplexity"]').val();
+      if (k == "") {
+        k = 5;
+        $('input[name="k"]').val(5);
+      }
+
+      if (dr == undefined) {
+        dr = 0;
+        $("input[name='dr'][value='0']").prop("checked",true);
+      }
+
+      if (variance == "") {
+        variance = 0.95
+        $('input[name="variance"]').val(0.95);
+      }
+
+      if (perplexity == "") {
+        perplexity = 30
+        $('input[name="perplexity"]').val(30);
+      }
+      params.push(k);
+      params.push(dr);
+      params.push(variance);
+      params.push(perplexity);
+
+      console.log(params);
+      var element = document.getElementById("km");
+      var r = await this.getData(element, params);
+      this.sendToApp();
+
+    }
+
+    else if (option_global == 'hc') {
+      console.log("hierarchical submitted");
+      var num = $('input[name="hc"]').val();
+      var linkage = $('input[name="linkage"]:checked').val();
+
+      if (num == "") {
+        num = 3
+        $('input[name="hc"]').val(3);
+      }
+
+      if (linkage == undefined) {
+        linkage = "ward"
+        $("input[name='linkage'][value='ward']").prop("checked",true);
+      }
+
+      params.push(num);
+      params.push(linkage);
+      console.log(params);
+
+      var element = document.getElementById("hc");
+      var r = await this.getData(element, params);
+      this.sendToApp();
+    }
+
+    else if (option_global == 'nc') {
+      console.log("no cluster submitted");
+      var maxmin = $('input[name="maxmin"]:checked').val();
+
+      if (maxmin == undefined) {
+        maxmin = "max"
+        $("input[name='maxmin'][value='max']").prop("checked",true);
+      }
+
+      params.push(maxmin);
+
+      element = document.getElementById("nc");
+      var r = await this.getData(element, params);
+      this.sendToApp();
+    }
+
+    else {
+      alert("Please select an option, thank you!")
+    }
   }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
 
+  selectedTechnique(id) {
+    var km = document.getElementById("km");
+    var hc = document.getElementById("hc");
+    var nc = document.getElementById("nc");
+    km.style.background = 'black';
+    hc.style.background = 'black';
+    nc.style.background = 'black';
+
+    var km_so = document.getElementById("km_so");
+    var hc_so = document.getElementById("hc_so");
+    var nc_so = document.getElementById("nc_so");
+    km_so.style.display = "none";
+    hc_so.style.display = "none";
+    nc_so.style.display = "none";
+
+    var div_id = document.getElementById(id);
+    div_id.style.background = 'teal'
+
+    if (id == "km") {
+      km_so.style.display = "block";
+      option_global = 'km';
+    }
+
+    if (id == "hc") {
+      hc_so.style.display = "block";
+      option_global = 'hc';
+    }
+
+    if (id == "nc") {
+      nc_so.style.display = "block";
+      option_global = 'nc';
+    }
+  }
+
+  showSubOptions_dr(id) {
+    var pca_so = document.getElementById("pca_subOptions");
+    var tsne_so = document.getElementById("tsne_subOptions");
+    pca_so.style.display = 'none';
+    tsne_so.style.display = 'none';
+
+    var div_id = document.getElementById(id);
+
+    if (id == "pca")
+      pca_so.style.display = "block";
+
+    if (id == "tsne")
+      tsne_so.style.display = "block";
+
+  }
+
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.value}
-        />
-        <button onClick={this.sendToBackend}>Send to backend</button>
-        <button onClick={this.sendToApp}>Send to app</button>
+      <div id="content">
+
+        <div id="options">
+          <div id="km" onClick={this.selectedTechnique.bind(this, 'km')} className="techniques">K-means</div>
+
+          <div id="hc" onClick={this.selectedTechnique.bind(this, 'hc')} className="techniques">Hierarchical</div>
+
+          <div id="nc" onClick={this.selectedTechnique.bind(this, 'nc')} className="techniques">No Clusters</div>
+
+          <div id="buttons">
+            <button onClick={this.sendToBackend}>Fetch Clusters</button>
+          </div>
+
+        </div>
+
+        <div id="suboptions" className="sub_options">
+
+            <div id="km_so" style={{display : 'none'}}>
+              <div>
+                <label>K: </label>
+                <input id = "k_val" type="text" name="k" />
+              </div>
+
+              <div>
+                <label>Dimensionality Reduction? </label><br></br>
+                <input type="radio" onClick={this.showSubOptions_dr.bind(this, 'pca')} id = "pca" value = "1" name="dr" />PCA
+                <input type="radio" onClick={this.showSubOptions_dr.bind(this, 'tsne')} id = "tsne" value = "2" name="dr" />t-SNE
+                <input type="radio" onClick={this.showSubOptions_dr.bind(this, 'no')} id = "no" value = "0" name="dr" />None
+              </div>
+
+              <div id="pca_subOptions" style={{display : 'none'}}>
+                <label>Variance %: </label>
+                <input id = "variance" type="text" name="variance" />
+              </div>
+
+              <div id="tsne_subOptions" style={{display : 'none'}}>
+                <label>Perplexity %: </label>
+                <input id = "perplexity" type="text" name="perplexity" />
+              </div>
+
+            </div>
+
+            <div id="hc_so" style={{display : 'none'}}>
+              <div>
+                <label># of clusters: </label>
+                <input id = "hc_val" type="text" name="hc" />
+              </div>
+
+              <div>
+                <div><label> Linkage:</label>
+                <input type="radio" id = "ward" value = "ward" name="linkage" />ward</div>
+                <div><input type="radio" id = "complete" value = "complete" name="linkage" />complete</div>
+                <div><input type="radio" id = "average" value = "average" name="linkage" />average</div>
+                <div><input type="radio" id = "single" value = "single" name="linkage" />single</div>
+              </div>
+
+            </div>
+
+            <div id="nc_so" style={{display : 'none'}}>
+              <div><label>Group by: </label></div>
+              <div>
+                <input type="radio" id = "max" value = "max" name="maxmin" />maximum
+                <input type="radio" id = "min" value = "min" name="maxmin" />minimum
+              </div>
+            </div>
+
+        </div>
+
+
       </div>
     );
   }
